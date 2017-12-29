2ce77d09077856178da20257d92444d3d0a1bc57
Alvaro Separovich <alvasepro@gmail.com>
2017-12-29 15:50:29 -0200

    escopo

--- app/views.py
+++ app/views.py
@@ -4,3 +4,11 @@ from app import app
 @app.route('/')
 def index():
     return render_template('main.html')
+
+@app.route('/siginup')
+def siginup():
+    return render_template('siginup.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
